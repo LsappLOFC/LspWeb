4de85c5ddb7fa6e289ece2727504dc41a1820193
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2022-10-07 19:38:57 -0500

    Panel de administrador completado

--- lspapp-web/src/app/Components/home/home.component.ts
+++ lspapp-web/src/app/Components/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {UsersService} from "../../services/users.service";
 import {User} from "../../interfaces/user";
+import {Router} from "@angular/router";
 declare var window: any;
 
 @Component({
@@ -12,11 +13,14 @@ declare var window: any;
 export class HomeComponent implements OnInit {
   searchText = "";
   listOfContacts:any ;
+  id = "";
+  habilitado: boolean = true;
 
   formModal: any;
   users:User[];
 
   constructor(private userService: UsersService,
+              private _router: Router,
               private http: HttpClient){
     this.users = [];
     this.userService.getUser().subscribe(data => {
@@ -32,17 +36,28 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  openFormModal() {
+  openFormModal(id:string, habilitado: boolean) {
     this.formModal.show();
-    this.userService.getUser().subscribe(data => {
-      data.forEach((user:any) => {
-        console.log(user.id);
-      })
-    });
+    this.id = id;
+    this.habilitado = habilitado;
   }
   saveSomeThing() {
     // confirm or save something
     this.formModal.hide();
+    if (this.habilitado){
+      this.habilitado = false
+      this.userService.updateUserHabilitado(this.id, this.habilitado);
+      console.log('cambio1');
+    }
+    else {
+      this.habilitado = true
+      this.userService.updateUserHabilitado(this.id, this.habilitado);
+      console.log('cambio2');
+    }
+  }
+
+  refresh(){
+    window.location.reload()
   }
 
   Search(){
--- lspapp-web/src/app/services/users.service.ts
+++ lspapp-web/src/app/services/users.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import {User} from "../interfaces/user";
 import {collection, collectionData, doc, Firestore, updateDoc} from "@angular/fire/firestore";
 import {Observable} from "rxjs";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor(private firestore: Firestore) { }
+  constructor(private firestore: Firestore,
+              private db: AngularFirestore) { }
 
   getUser(): Observable<User[]> {
     const placeRef = collection(this.firestore, 'users');
     return collectionData(placeRef, { idField: 'id' }) as Observable<User[]>;
+  }
+  updateUserHabilitado(_id:any, _habilitado:boolean) {
+    this.db.doc(`users/${_id}`).update({habilitado:_habilitado});
   }
 }
