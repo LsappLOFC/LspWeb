6c4671cb7d5d1b2278807b16c6d61abe5d570291
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2022-10-10 17:50:28 -0500

    Actualizacion de Sugerencias

--- lspapp-web/src/app/Components/sugerencias/sugerencias.component.ts
+++ lspapp-web/src/app/Components/sugerencias/sugerencias.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CommentsService} from "../../services/comments.service";
 import {UsersService} from "../../services/users.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Comments} from "../../interfaces/comments";
+import {User} from "../../interfaces/user";
+
 
 @Component({
   selector: 'app-sugerencias',
@@ -11,10 +11,12 @@ import {Comments} from "../../interfaces/comments";
 })
 export class SugerenciasComponent implements OnInit {
   listOfComents:any;
-  listOfContacts:any;
+  listOfUsers:any;
   comments: unknown;
   searchText = "";
   date = '';
+
+  users: any;
 
   constructor(private commentService: CommentsService,
               private userService: UsersService) {
@@ -22,36 +24,33 @@ export class SugerenciasComponent implements OnInit {
     this.commentService.getComments().subscribe(data => {
       this.listOfComents = data;
     }, error => console.error(error));
-
-    this.userService.getUser().subscribe(data => {
-      this.listOfContacts = data;
-    }, error => console.error(error));
   }
 
   ngOnInit(): void {
+    this.userService.getUser().subscribe(data => {
+      this.listOfUsers = data;
+    }, error => console.error(error));
   }
 
-  changeStatusLeido(id: string, estado_leido: boolean) {
-    if (estado_leido){
-      alert('El mensaje esta marcado como leido, no se puede hacer cambios');
-    }
-    else {
-      estado_leido = true
-      this.commentService.updateCommentsLeido(id, estado_leido);
-    }
+  transform(items: any[], searchText: string): any[] {
+    if(!items) return [];
+    if(!searchText) return items;
+
+    searchText = searchText.toLowerCase();
+    return items.filter( it => {
+      return it.name.toLowerCase().includes(searchText);
+    });
   }
+
+
 
   Search(){
     // alert(this.searchText)
     if(this.searchText!== ""){
-      let searchValue = this.searchText.toString();
+      let searchValue = this.searchText.toLocaleLowerCase();
 
       this.listOfComents = this.listOfComents.filter((contact:any) =>{
-        if(searchValue == contact.fechaHoraRegistro){
-          return contact.fechaHoraRegistro
-        }
-        // you can keep on adding object properties here
-
+        return contact.comment.toLocaleLowerCase().match(searchValue);
       });
 
       console.log(this.listOfComents);
@@ -66,4 +65,24 @@ export class SugerenciasComponent implements OnInit {
     }
   }
 
+
+  changeStatusLeido(id: string, estado_leido: boolean) {
+    if (estado_leido){
+      alert('El mensaje esta marcado como LEIDO, no se puede hacer cambios');
+    }
+    else {
+      estado_leido = true
+      this.commentService.updateCommentsLeido(id, estado_leido);
+    }
+  }
+
+  changeStatusLeidoToNoLeido(id: string, estado_leido: boolean) {
+    if (!estado_leido){
+      alert('El mensaje esta marcado como NO LEIDO, no se puede hacer cambios');
+    }
+    else {
+      estado_leido = false
+      this.commentService.updateCommentsLeido(id, estado_leido);
+    }
+  }
 }
--- lspapp-web/src/app/interfaces/comments.ts
+++ lspapp-web/src/app/interfaces/comments.ts
@@ -1,3 +1,5 @@
+import {User} from "./user";
+
 export interface Comments {
   id?: string,
   comment: string,
